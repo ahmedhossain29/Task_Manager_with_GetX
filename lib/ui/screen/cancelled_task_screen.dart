007b56ee03e8f07a13d8173fb54bfba3395f6285
AHMED <hossainahmad76@gmail.com>
2023-12-08 23:21:03 +0600

    Cancelled Task screen

--- lib/ui/screen/cancelled_task_screen.dart
+++ lib/ui/screen/cancelled_task_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:taskmanagerwithgetx/ui/controllers/cancelled_task_controller.dart';
 
 import '../../data_network_caller/models/task_list_model.dart';
-import '../../data_network_caller/network_caller.dart';
-import '../../data_network_caller/network_response.dart';
-import '../../data_network_caller/utility/urls.dart';
 import '../widgets/profile_widget.dart';
 import '../widgets/task_item_card.dart';
 
@@ -15,30 +14,13 @@ class CancelledTaskScreen extends StatefulWidget {
 }
 
 class _CancelledTaskScreenState extends State<CancelledTaskScreen> {
-  bool getCancelledTaskInProgress = false;
   TaskListModel taskListModel = TaskListModel();
-
-  Future<void> getCancelledTaskList() async {
-    getCancelledTaskInProgress = true;
-    if (mounted) {
-      setState(() {});
-    }
-    final NetworkResponse response =
-        await NetworkCaller().getRequest(Urls.getCancelledTasks);
-    if (response.isSuccess) {
-      taskListModel = TaskListModel.fromJson(response.jsonResponse);
-    }
-    getCancelledTaskInProgress = false;
-    if (mounted) {
-      setState(() {});
-    }
-  }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getCancelledTaskList();
+    Get.find<CancelledTaskController>().getCancelledTaskList();
   }
 
   @override
@@ -49,30 +31,32 @@ class _CancelledTaskScreenState extends State<CancelledTaskScreen> {
           children: [
             const ProfileSummaryCard(),
             Expanded(
-              child: Visibility(
-                visible: getCancelledTaskInProgress == false,
-                replacement: const Center(child: CircularProgressIndicator()),
-                child: RefreshIndicator(
-                  onRefresh: getCancelledTaskList,
-                  child: ListView.builder(
-                    itemCount: taskListModel.taskList?.length ?? 0,
-                    itemBuilder: (context, index) {
-                      return TaskItemCard(
-                        task: taskListModel.taskList![index],
-                        onStatusChange: () {
-                          getCancelledTaskList();
-                        },
-                        showProgress: (inProgress) {
-                          getCancelledTaskInProgress = inProgress;
-                          if (mounted) {
-                            setState(() {});
-                          }
-                        },
-                      );
-                    },
+              child: GetBuilder<CancelledTaskController>(
+                  builder: (cancelledTaskController) {
+                return Visibility(
+                  visible: cancelledTaskController.getCancelledTaskInProgress ==
+                      false,
+                  replacement: const Center(child: CircularProgressIndicator()),
+                  child: RefreshIndicator(
+                    onRefresh: cancelledTaskController.getCancelledTaskList,
+                    child: ListView.builder(
+                      itemCount: cancelledTaskController
+                              .taskListModel.taskList?.length ??
+                          0,
+                      itemBuilder: (context, index) {
+                        return TaskItemCard(
+                          task: cancelledTaskController
+                              .taskListModel.taskList![index],
+                          onStatusChange: () {
+                            cancelledTaskController.getCancelledTaskList();
+                          },
+                          showProgress: (inProgress) {},
+                        );
+                      },
+                    ),
                   ),
-                ),
-              ),
+                );
+              }),
             ),
           ],
         ),
